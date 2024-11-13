1ae1fe1e678e273fd09982f34a6c4ecfca76e787
iHtAlgorithm <y1785623455461@gmail.com>
2024-11-13 19:25:02 +0800

    caluate

--- week10-3/main.cpp
+++ week10-3/main.cpp
@@ -1,6 +1,8 @@
 #include <iostream>
-
+#include <cstring>
+#include <cctype>
 using namespace std;
+char str[370];
 
 struct HuffmanNode
 {
@@ -9,10 +11,32 @@ struct HuffmanNode
     HuffmanNode* left;
     HuffmanNode* right;
 };
-
+void caluate(char str[], HuffmanNode Tree)
+{
+    int len = strlen(str);
+    int cal[26];
+    memset(cal, 0, sizeof(cal));
+    for (size_t i = 0; i < len; i++)
+    {
+        str[i] = tolower(str[i]);
+        cal[str[i] - 'a']++;
+    }
+    int index = 0;
+    for (size_t i = 0; i < len; i++)
+    {
+        if (str[i] != '0')
+        {
+            Tree[index].value = str[i];
+            Tree[index].freq = cal[str[i] - 'a'];
+        }
+        index++;
+    }
+    
+}
 int main()
 {
-    
+    cin >> str;
+    HuffmanNode Tree[368];
     
     
     system("pause");
